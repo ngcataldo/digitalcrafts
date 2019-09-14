98e09399379ee88c63e776ee62151d6fb19af71d
Natalie Cataldo <ngcataldo@gmail.com>
2019-09-14 13:18:59 -0400

    First attempt at age exercise

---
+++ age_verification.js
@@ -0,0 +1,39 @@
+var canDrive= function(age)  {
+
+   if(age > 16)  {
+     return tru
+   } else  {
+     return false
+ }
+
+}
+
+var canDrink= function(age)  {
+
+   return age >= 21;
+}
+
+ console.log(canDrink(16)) 
+
+
+
+var canRent= function(age)  {
+  
+   return age >= 25;
+
+}
+
+console.log(canRent(27))
+
+
+var canPlay= function(age)  {
+     return age <= 10;
+
+}
+
+console.log(canPlay(11))
+
+
+//Step 2
+
+
