e8c65a57fd041c4887d3a3c4af9f2a47502d0fbf
Natalie Cataldo <ngcataldo@gmail.com>
2019-09-17 16:25:09 -0400

    Finished Saturdays assignment

--- age_verification.js
+++ age_verification.js
@@ -1,39 +1,116 @@
+// STEP 1
+// function should test out if person is old enough to drive
+
 var canDrive= function(age)  {
 
-   if(age > 16)  {
-     return tru
-   } else  {
-     return false
- }
+	if(age >=  16)  {
+     		return true;
+	}
+
+     	return false;
 
 }
+
+  console.log(canDrive(22));
+
+// function should test out if a person can drink 
 
 var canDrink= function(age)  {
 
-   return age >= 21;
-}
+   if(age >= 21)  {
+     return true;
 
- console.log(canDrink(16)) 
+   }
+     
+     return false
 
+   }
 
+// test example console.log(canDrink(22));
+
+// function should test out if they can play in the McDonald's ball pit
+
+var canPlayInBallPit= function(age)   {
+     
+      if(age <= 10)  {
+      	return true;
+
+   } 
+     
+     return false
+
+   }  
+
+// test example console.log(canPlayInBallPit(9));
+
+// function should test out if a person can rent a car
 
 var canRent= function(age)  {
   
-   return age >= 25;
+      if(age >= 25)  {
+      	return true;
 
-}
+   }
 
-console.log(canRent(27))
+     return false
+
+   }
+ 
+// test example console.log(canRent(27));
+
+// STEP 2 
+// creating setup to test out if ages are true or false
+
+var resultArray = [];
+
+var testAges = function(name, age)  {
+	
+  var message = "";
+
+  if (canDrive(age))  {
+     message = name + " can drive";
+  }  else  {
+     message = name + " cannot drive";
+  }
+
+  resultArray.push(message);
 
 
-var canPlay= function(age)  {
-     return age <= 10;
+   if (canDrink(age))  {
+        message = name +  " can drink";
+   }  else  {
+     	  message = name +  " cannot drink";
+   }
 
-}
+  
+   resultArray.push(message);
 
-console.log(canPlay(11))
+   
+   if (canPlayInBallPit(age))  {
+      message = name +  " can play in the ball pit";
+   }  else  {
+      message = name +  " cannot play in the ball pit";
+   }
 
 
-//Step 2
+   resultArray.push(message);
 
 
+   if (canRent(age))  {
+      message = name +  "  can rent";
+   }  else  {
+      message = name +  "  can not rent";
+   }
+
+  
+  resultArray.push(message);
+
+};
+
+testAges("Sarah", 35);  
+testAges("Bob", 19);
+testAges("Henry", 22);
+testAges("Katie", 9);
+
+
+console.log(resultArray);
